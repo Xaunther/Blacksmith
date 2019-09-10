217baf81d8f8857b6722ca01d2f418161f959bf3
Xaunther <alex.1993.24@gmail.com>
2019-09-10 11:42:35 +0200

    Save also remaining pieces on the board

--- SaveHistory.cxx
+++ SaveHistory.cxx
@@ -17,6 +17,21 @@ void SaveHistory(destination history[36], int steps, tableau* board)
   for(int i=0;i<steps;i++)
     {
       outfile << setw(4) << i << "     (" << history[i].x << "," << history[i].y << ")     " << setw(1) << board->pieces[history[i].x][history[i].y] << endl;
+      board->pieces[history[i].x][history[i].y] = "E";
+    }
+  //Write also remaining pieces
+  outfile << "----------------------" << endl;
+  outfile << "   Remaining Pieces   " << endl;
+  outfile << "----------------------" << endl;
+  for(int i=0;i<6;i++)
+    {
+      for(int j=0;j<6;j++)
+	{
+	  if(board->pieces[i][j] != "E")
+	    {
+	      outfile << "(" << i << "," << j << "): " << board->pieces[i][j] << endl;
+	    }
+	}
     }
   outfile.close();
 }
