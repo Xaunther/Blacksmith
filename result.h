0b02b4e523d4e628d8e5f095e562d5df609f2727
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-08-29 17:31:53 +0200

    Count score for best result

--- result.h
+++ result.h
@@ -16,6 +16,8 @@ public:
 
 	void FindBest( const tableau& aInitialBoard, const history::value_type& aInitialPosition, const unsigned int& aNTries, std::mt19937_64& aRNG );
 
+	bool IsBetterResult( const history::size_type& aCountHits, const unsigned short& aScore ) const;
+
 private:
 	history mHistory;
 	tableau mTableau;
