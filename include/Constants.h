21533116b044ea779fb2b23e73cd52ebff790bfa
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-11-27 16:18:53 +0100

    Use constexpr for the constants

--- include/Constants.h
+++ include/Constants.h
@@ -3,12 +3,12 @@
 namespace blacksmith
 {
 
-static const unsigned short ROW = 6;
-static const unsigned short COL = 6;
-static const unsigned short MAXPOS = ROW * COL;
-static const unsigned short MOVEPOS = 4;
-static const unsigned short MOVEPOS_K = MOVEPOS * 2;
-static const unsigned long MAXSTEPS = 10000000;
-static const unsigned short N_THREADS = 8;
+constexpr unsigned short ROW = 6;
+constexpr unsigned short COL = 6;
+constexpr unsigned short MAXPOS = ROW * COL;
+constexpr unsigned short MOVEPOS = 4;
+constexpr unsigned short MOVEPOS_K = MOVEPOS * 2;
+constexpr unsigned long MAXSTEPS = 10000000;
+constexpr unsigned short N_THREADS = 8;
 
 }
