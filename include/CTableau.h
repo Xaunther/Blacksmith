232215bdbf316015f1942cbc027703f0c9cd1283
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-04 12:38:04 +0100

    Use enums instead of strings for piece type

--- include/CTableau.h
+++ include/CTableau.h
@@ -13,12 +13,27 @@ namespace blacksmith
 class CTableau
 {
 public:
-	using pieces = std::vector<std::string>;
+	enum class E_PIECE_TYPE
+	{
+		EMPTY,
+		ONE = 1,
+		TWO = 2,
+		THREE = 3,
+		FOUR = 4,
+		QUEEN,
+		BISHOP,
+		ROOK,
+		KNIGHT,
+		WILDCARD
+	};
+
+	using piece_type = E_PIECE_TYPE;
+	using pieces = std::vector<piece_type>;
 	using index = pieces::size_type;
 
 	explicit CTableau( const std::string& aFileName );
 
-	const std::string& GetPiece( const index& aRowIndex, const index& aColIndex ) const;
+	const piece_type& GetPiece( const index& aRowIndex, const index& aColIndex ) const;
 
 	const index& GetRows() const;
 
@@ -29,9 +44,12 @@ public:
 	index CountPieces() const;
 	index Size() const;
 
-	static const std::set<std::string>& CHESS_PIECES();
-	static const std::set<std::string>& NUMBER_PIECES();
-	static const std::set<std::string>& PIECES();
+	static std::string PieceToString( const E_PIECE_TYPE& aPiece );
+	static E_PIECE_TYPE StringToPiece( const std::string& aPiece );
+
+	static const std::set<piece_type>& CHESS_PIECES();
+	static const std::set<piece_type>& NUMBER_PIECES();
+	static const std::set<piece_type>& PIECES();
 
 private:
 	// Variables
